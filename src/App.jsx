3fc95fc28d66be0f88952f0746a539e48c9b8266
MD ABDULLAH <mdabdullah161036@gmail.com>
2024-02-11 19:00:09 +0600

    successfully sent data to db

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,35 @@
 import "./App.css";
 
 function App() {
+  const handleUser = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const user = { name, email };
+    console.log(user);
+    fetch("http://localhost:5000/users", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  };
   return (
     <div>
       <h1>Simple CRUD Operation</h1>
+      <form onSubmit={handleUser}>
+        <input type='text' name='name' id='' placeholder='Name' />
+        <br />
+        <input type='email' name='email' id='' placeholder='Email' />
+        <br />
+        <input type='submit' value='Add User' />
+      </form>
     </div>
   );
 }
